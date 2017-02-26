e542aa0d0044d6ac9b530c0e4433ef0bd15e4211
nathan-beneke <benek020@morris.umn.edu>
2017-02-25 20:43:02 -0600

    Update this.todo in todo-list-component. Still need to figure out the display of that data

--- client/src/app/todos/todo-list.component.ts
+++ client/src/app/todos/todo-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { TodoListService } from "./todo-list.service";
 import { Todo } from "./todo";
 import { FilterBy } from "./filter.pipe";
+import {Observable} from "rxjs";
+import {forEach} from "@angular/router/src/utils/collection";
 
 @Component({
     selector: 'todo-list-component',
@@ -11,6 +13,7 @@ import { FilterBy } from "./filter.pipe";
 
 export class TodoListComponent implements OnInit {
     public todos: Todo[];
+    public worked = false;
 
     constructor(private todoListService: TodoListService) {
         //this.todos = this.todoListService.getTodos();
@@ -23,5 +26,26 @@ export class TodoListComponent implements OnInit {
                 console.log(err);
             }
         );
+    }
+
+    public clicked(owner: string, status: string, category: string, body: string): void {
+        let i = 0;
+        let todoArray: Todo[] = [];
+
+        this.todoListService.filterTodos("Blanche", "sunt", "incomplete", "software design").subscribe(
+            todos => todoArray[i++] = todos,
+            err => {
+                console.log(err);
+            }
+        );
+        i = 0;
+
+        for (let todo of todoArray){
+            this.todos[i++] = todo;
+        }
+
+
+        console.log(todoArray);
+        console.log(this.todos);
     }
 }
--- client/src/app/todos/todo-list.service.ts
+++ client/src/app/todos/todo-list.service.ts
@@ -3,9 +3,10 @@ import { Http } from '@angular/http';
 import { Todo } from './todo';
 import { Observable } from "rxjs";
 
+
 @Injectable()
 export class TodoListService {
-    private todoUrl: string = API_URL + "todos?";
+    private todoUrl: string = API_URL + "todos";
     constructor(private http:Http) { }
 
 
@@ -15,5 +16,14 @@ export class TodoListService {
 
     getTodoById(id: string): Observable<Todo> {
         return this.http.request(this.todoUrl + "/" + id).map(res => res.json());
+    }
+
+    filterTodos(owner: string, body: string, status: string, category: string): Observable<Todo> {
+        var todos = this.http.request(this.todoUrl + "?" + "owner=" + owner + "&status=" + status + "&body=" + body +
+            "&category=" + category).map(res => res.json());
+
+
+
+        return todos;
     }
 }
