35b2862e45a6027cb3813ad89368162468aad854
nathan-beneke <benek020@morris.umn.edu>
2017-02-27 19:25:42 -0600

    Add display of progress by owners and categories
    Also improve the general formatting of the summary section.

--- client/src/app/todos/todo-summary.component.ts
+++ client/src/app/todos/todo-summary.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 import {TodoSummary} from "./todo-summary";
-import {Observable} from "rxjs";
 
 
 @Component({
@@ -15,12 +14,23 @@ export class TodoSummaryComponent implements OnInit {
         categoriesPercentComplete: new Map<string,number>(),
         ownersPercentComplete: new Map<string,number>()
     };
+    public owners: string[] = [];
+    public categories: string[] = [];
+
     constructor(private http:Http) {
     }
 
     ngOnInit(): void {
         this.http.request(API_URL + "todoSummary").map(res => res.json()).subscribe(
-            todoSum => this.todoSummary = todoSum,
+            todoSum => {
+                this.todoSummary = todoSum;
+                for (let key in this.todoSummary.ownersPercentComplete){
+                    this.owners.push(key);
+                }
+                for (let key in this.todoSummary.categoriesPercentComplete){
+                    this.categories.push(key);
+                }
+            },
             err => {
 
                 console.log(err);
